7d12e0cb5dbdcddc56ea0a01da4869a029baf953
SibusisoMad <smadloba@gmail.com>
2025-09-22 12:37:37 +0200

    initial commit:  configuration fix

--- InterviewApp/Program.cs
+++ InterviewApp/Program.cs
@@ -3,17 +3,23 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using InterviewApp.Services;
+using InterviewApp.Models;
 
 class Program
 {
     static async Task Main(string[] args)
     {
         using IHost host = Host.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((context, config) =>
+            //.ConfigureAppConfiguration((context, config) =>
+            //{
+            //    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            //})
+            .ConfigureServices((context, services) =>
             {
-                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                services.Configure<GreetingOptions>(context.Configuration.GetSection("Greeting"));
+
+                services.AddTransient<IGreetingService, GreetingService>();
             })
-            .ConfigureServices((_, services) => { services.AddTransient<IGreetingService, GreetingService>(); })
             .Build();
 
         var greetingService = host.Services.GetRequiredService<IGreetingService>();
